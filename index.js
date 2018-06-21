be67d0a363f8f85daa521f6be46119cd07782865
Angel Laguna <alagunag29@gmail.com>
2018-06-21 15:53:18 -0500

    completado

--- index.js
+++ index.js
@@ -1,53 +1,81 @@
 var data = [
     {
         product   : 'Sandalia de Cuero y  Tachas saville Row',
-        urlImage  : './p1.png',
+        urlImage  : './assets/p1.png',
         talla     : 'XS',
-        precio    : 40.000,
-        urlPerfil : './profile.png',
+        price     : '$40.000',
+        urlPerfil : './assets/profile.png',
         perfil    : 'Straadivaruois & Zara Home Juntos'
     },
     {
         product   : 'Sandalia de Cuero y  Tachas saville Row',
-        urlImage  : './p2.png',
+        urlImage  : './assets/p2.png',
         talla     : 'XS',
-        precio    : 40.000,
-        urlPerfil : './profile.png',
+        price    :  '$40.000',
+        urlPerfil : './assets/profile.png',
         perfil    : 'Straadivaruois & Zara Home Juntos'
     },
     {
         product   : 'Sandalia de Cuero y  Tachas saville Row',
-        urlImage  : './p3.png',
+        urlImage  : './assets/p3.png',
         talla     : 'XS',
-        precio    : 40.000,
-        urlPerfil : './profile.png',
+        price     : '$40.000',
+        urlPerfil : './assets/profile.png',
         perfil    : 'Straadivaruois & Zara Home Juntos'
     },
     {
         product   : 'Sandalia de Cuero y  Tachas saville Row',
-        urlImage  : './p4.png',
+        urlImage  : './assets/p4.png',
         talla     : 'XS',
-        precio    : 40.000,
-        urlPerfil : './profile.png',
+        price     : '$40.000',
+        urlPerfil : './assets/profile.png',
         perfil    : 'Straadivaruois & Zara Home Juntos'
     },
     {
         product   : 'Sandalia de Cuero y  Tachas saville Row',
-        urlImage  : './p5.png',
+        urlImage  : './assets/p5.png',
         talla     : 'XS',
-        precio    : 40.000,
-        urlPerfil : './profile.png',
+        price     : '$40.000',
+        urlPerfil : './assets/profile.png',
         perfil    : 'Straadivaruois & Zara Home Juntos'
     },
     {
         product   : 'Sandalia de Cuero y  Tachas saville Row',
-        urlImage  : './p6.png',
+        urlImage  : './assets/p6.png',
         talla     : 'XS',
-        precio    : 40.000,
-        urlPerfil : './profile.png',
+        price     : '$40.000',
+        urlPerfil : './assets/profile.png',
         perfil    : 'Straadivaruois & Zara Home Juntos'
     }
-
 ];
 
-console.log(data);
+$(document).ready(function(){
+    for (var i = 0; i < data.length; i++) {
+        $(".container-content").append(
+            "<div class='container-product'>" +
+                "<div class='container-image'>" +
+                    "<img src='"+ data[i].urlImage+"'>" +
+                "</div>"+
+                "<div class='content-details'>"+
+                    "<div class='container-name-product'>"+
+                        "<span class='name-product'>"+ data[i].product + "</span>"+
+                    "</div>"+
+                    "<div class='container-talla'>"+
+                        "<span class='talla-product'>"+ data[i].talla + "</span>"+
+                    "</div>" +
+                "</div>" +
+                "<div class='content-info'>"+
+                    "<div class='text-info'>" +
+                        "<span class='info-product'>" + data[i].perfil + "</span>" +
+                    "</div>"+
+                    "<div><span class='preci-product'>" + data[i].price + "</span></div>"+
+                "</div>"+
+                "<div class='content-icon'>" +
+                    "<div class='icon-profile'>"+
+
+                    "</div>"+
+                "</div>"+
+            "</div>"
+        );
+    }
+});
