04f9a76f2465fc4b2cb5e959f423ebd25d84986e
Angel Laguna <alagunag29@gmail.com>
2018-06-21 03:39:30 -0500

    array de datos, css y html con titulo

---
+++ index.js
@@ -0,0 +1,53 @@
+var data = [
+    {
+        product   : 'Sandalia de Cuero y  Tachas saville Row',
+        urlImage  : './p1.png',
+        talla     : 'XS',
+        precio    : 40.000,
+        urlPerfil : './profile.png',
+        perfil    : 'Straadivaruois & Zara Home Juntos'
+    },
+    {
+        product   : 'Sandalia de Cuero y  Tachas saville Row',
+        urlImage  : './p2.png',
+        talla     : 'XS',
+        precio    : 40.000,
+        urlPerfil : './profile.png',
+        perfil    : 'Straadivaruois & Zara Home Juntos'
+    },
+    {
+        product   : 'Sandalia de Cuero y  Tachas saville Row',
+        urlImage  : './p3.png',
+        talla     : 'XS',
+        precio    : 40.000,
+        urlPerfil : './profile.png',
+        perfil    : 'Straadivaruois & Zara Home Juntos'
+    },
+    {
+        product   : 'Sandalia de Cuero y  Tachas saville Row',
+        urlImage  : './p4.png',
+        talla     : 'XS',
+        precio    : 40.000,
+        urlPerfil : './profile.png',
+        perfil    : 'Straadivaruois & Zara Home Juntos'
+    },
+    {
+        product   : 'Sandalia de Cuero y  Tachas saville Row',
+        urlImage  : './p5.png',
+        talla     : 'XS',
+        precio    : 40.000,
+        urlPerfil : './profile.png',
+        perfil    : 'Straadivaruois & Zara Home Juntos'
+    },
+    {
+        product   : 'Sandalia de Cuero y  Tachas saville Row',
+        urlImage  : './p6.png',
+        talla     : 'XS',
+        precio    : 40.000,
+        urlPerfil : './profile.png',
+        perfil    : 'Straadivaruois & Zara Home Juntos'
+    }
+
+];
+
+console.log(data);
